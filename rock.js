64a8b0a034de8942129b9d57836d3b9e16fd5f05
ThatSimplex <jonas.folvik@gmail.com>
2024-12-16 11:54:52 +0100

    Able to play 1 round

--- rock.js
+++ rock.js
@@ -12,9 +12,35 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let choice = prompt("Rock, Paper or Scissors?").toLowerCase();
-    if (choice != "rock" || choice != "paper" || choice != "scissors") {
+    if (choice != "rock" && choice != "paper" && choice != "scissors") {
         console.log("Not a valid option");
         getHumanChoice();
     }
     return choice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        console.log("It's a draw!");
+    } else if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You lost!");
+        computerScore += 1;
+    } else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You lost!");
+        computerScore += 1;
+    } else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You lost!");
+        computerScore += 1;
+    } else {
+        console.log("You won!");
+        humanScore += 1;
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
